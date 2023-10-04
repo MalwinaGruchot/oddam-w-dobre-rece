31688b1ea915c3acc3946308cfb108d644fe25e3
MalwinaGruchot <malwinagruchot@wp.pl>
2023-10-04 16:49:40 +0200

    decor

---
+++ src/components/TextAndDecor.jsx
@@ -0,0 +1,14 @@
+import decor from "../assets/Decoration.svg";
+import styles from "./TextAndDecor.module.scss";
+
+export default function TextAndDecor({ text1, text2 }) {
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>{text1}</h2>
+      {text2 ? <h2 className={styles.title}>{text2}</h2> : null}
+      <div className={styles.decor}>
+        <img src={decor} />
+      </div>
+    </div>
+  );
+}
