2fedd7427de874ef1044e295193480e9092eae94
MalwinaGruchot <malwinagruchot@wp.pl>
2023-10-04 16:46:10 +0200

    make router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import RouteWrapper from "./RouteWrapper";
+import LogIn from "./LogIn";
+import CreateAnAccount from "./CreateAnAccount";
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RouteWrapper />}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<LogIn />}></Route>
+            <Route
+              path="/create-an-account"
+              element={<CreateAnAccount />}
+            ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
