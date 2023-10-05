aa5d8538dbbe59326a08790e34147012b5b9e360
MalwinaGruchot <malwinagruchot@wp.pl>
2023-10-05 14:43:53 +0200

    data

---
+++ src/data/data.jsx
@@ -0,0 +1,124 @@
+export const data = [
+  {
+    id: "fundacja",
+    description:
+      "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+    items: [
+      {
+        id: 1,
+        name: `Fundacja "Dbam o Zdrowie"`,
+        decription:
+          "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+        items: ["ubrania", "jedzenie", "sprzęt AGD", "meble", "zabawki"],
+      },
+      {
+        id: 2,
+        name: `Fundacja “Dla dzieci”`,
+        decription: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+        items: ["ubrania", "meble", "zabawki"],
+      },
+      {
+        id: 3,
+        name: `Fundacja “Bez domu”`,
+        decription:
+          "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+        items: ["ubrania", "jedzenie", "ciepłe koce"],
+      },
+      {
+        id: 4,
+        name: `Fundacja "Dbam o Zdrowie"`,
+        decription:
+          "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+        items: ["ubrania", "jedzenie", "sprzęt AGD", "meble", "zabawki"],
+      },
+      {
+        id: 5,
+        name: `Fundacja “Dla dzieci”`,
+        decription: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+        items: ["ubrania", "meble", "zabawki"],
+      },
+      {
+        id: 6,
+        name: `Fundacja “Bez domu”`,
+        decription:
+          "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+        items: ["ubrania", "jedzenie", "ciepłe koce"],
+      },
+      {
+        id: 7,
+        name: `Fundacja “Dla dzieci”`,
+        decription: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+        items: ["ubrania", "meble", "zabawki"],
+      },
+      {
+        id: 8,
+        name: `Fundacja “Bez domu”`,
+        decription:
+          "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+        items: ["ubrania", "jedzenie", "ciepłe koce"],
+      },
+    ],
+  },
+  {
+    id: "organizacja",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+    items: [
+      {
+        id: 1,
+        name: `Organizacja “Lorem Ipsum 1”`,
+        decription:
+          "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+        items: ["ubrania", "jedzenie", "sprzęt AGD"],
+      },
+      {
+        id: 2,
+        name: `Organizacja “Lorem Ipsum 2”`,
+        decription:
+          "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+        items: ["ubrania", "jedzenie"],
+      },
+      {
+        id: 3,
+        name: `Organizacja “Lorem Ipsum 3”`,
+        decription:
+          "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+        items: ["ubrania", "jedzenie"],
+      },
+      {
+        id: 4,
+        name: `Organizacja “Lorem Ipsum 4”`,
+        decription:
+          "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+        items: ["Mi", "quis", "hendrerit", "dolor"],
+      },
+    ],
+  },
+  {
+    id: "zbiorka",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+    items: [
+      {
+        id: 1,
+        name: `Zbiórka “Lorem Ipsum 1”`,
+        decription:
+          "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+        items: ["Egestas", "sed", "tempus"],
+      },
+      {
+        id: 2,
+        name: `Zbiórka “Lorem Ipsum 2”`,
+        decription:
+          "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+        items: ["Egestas", "sed", "tempus"],
+      },
+      {
+        id: 3,
+        name: `Zbiórka “Lorem Ipsum 2”`,
+        decription: "Scelerisque in dictum non consectetur a erat nam.",
+        items: ["Egestas", "sed", "tempus"],
+      },
+    ],
+  },
+];
